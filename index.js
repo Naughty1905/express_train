bf7b3110193abfb96797c2c67f495a12a72d22a8
Naughty1905 <minion1905@gmail.com>
2022-04-06 01:03:14 +0300

    make response from html files

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
         res.writeHead(200,{
-            'Content-Type': 'text/html',
+            'Content-Type': 'text/html; charset=utf-8',
         })
-        res.end(`
-        <h1>Form</h1>
-        <form method="post" action="/">
-            <input type="text" name="title">
-            <button type="submit">Send</button>
-        </form>
-        `)
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, file) => {
+                if (err) throw err;
+                res.end(file);
+            })
+        }
+        if (req.url === '/about') {
+            fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, file) => {
+                if (err) throw err;
+                res.end(file);
+            })
+        }
     } else if (req.method === 'POST') {
         res.writeHead(200,{
             'Content-Type': 'text/html; charset=utf-8',
@@ -23,7 +30,10 @@ const server = http.createServer((req, res) => {
 
         req.on('end', () => {
             const message = body.toString().split('=')[1];
-            res.end(`<h1>Your name: ${message}</h1>`);
+            res.end(`
+                <h1>Your name: ${message}</h1>
+                <a href="/">To main page</a>
+            `);
         })
     }
 });
